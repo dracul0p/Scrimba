1731e8dd2bf0b07fa5a528ca79e42815cf6e31ad
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-02-26 23:25:51 +0530

    Restructured react-basics folder

---
+++ react-basics/01_travel/src/components/Header.jsx
@@ -0,0 +1,11 @@
+// src/components/Header.jsx
+
+export default function Header() {
+  return(
+    <header>
+      <img src="src/assets/globe.png" alt ="globe" />
+      <h1>My Travel journel</h1>
+    </header>
+  )
+}
+ 
